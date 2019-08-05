da1ed1d3cf059f1727e59df1b97cde60d2432e4d
Simon Sawert <simon@sawert.se>
2019-08-05 16:15:47 +0200

    Add missing letters
    * Add letters as 2D arrays
    * Add support to convert 2D array to offset array
    * Add method to test print 2D array as ASCII character
    
    This will make it easier to add new characters in the future since the
    2D array's design can visaulise how the GitHub grid looks.

--- commit.py
+++ commit.py
@@ -12,6 +12,245 @@ day = int(sys.argv[3])
 startDate = datetime.date(year, month, day)
 
 
+def all_letters():
+    return {
+        "a": [
+            [0, 1, 1, 0],
+            [1, 0, 0, 1],
+            [1, 0, 0, 1],
+            [1, 1, 1, 1],
+            [1, 0, 0, 1],
+            [1, 0, 0, 1],
+            [1, 0, 0, 1],
+        ],
+        "b": [
+            [1, 1, 1, 0],
+            [1, 0, 0, 1],
+            [1, 0, 0, 1],
+            [1, 1, 1, 0],
+            [1, 0, 0, 1],
+            [1, 0, 0, 1],
+            [1, 1, 1, 0],
+        ],
+        "c": [
+            [0, 1, 1, 0],
+            [1, 0, 0, 1],
+            [1, 0, 0, 0],
+            [1, 0, 0, 0],
+            [1, 0, 0, 0],
+            [1, 0, 0, 1],
+            [0, 1, 1, 0],
+        ],
+        "d": [
+            [1, 1, 1, 0],
+            [1, 0, 0, 1],
+            [1, 0, 0, 1],
+            [1, 0, 0, 1],
+            [1, 0, 0, 1],
+            [1, 0, 0, 1],
+            [1, 1, 1, 0],
+        ],
+        "e": [
+            [1, 1, 1, 1],
+            [1, 0, 0, 0],
+            [1, 0, 0, 0],
+            [1, 1, 1, 0],
+            [1, 0, 0, 0],
+            [1, 0, 0, 0],
+            [1, 1, 1, 1],
+        ],
+        "f": [
+            [1, 1, 1, 1],
+            [1, 0, 0, 0],
+            [1, 0, 0, 0],
+            [1, 1, 1, 0],
+            [1, 0, 0, 0],
+            [1, 0, 0, 0],
+            [1, 0, 0, 0],
+        ],
+        "g": [
+            [0, 1, 1, 0],
+            [1, 0, 0, 1],
+            [1, 0, 0, 0],
+            [1, 0, 0, 0],
+            [1, 0, 1, 1],
+            [1, 0, 0, 1],
+            [0, 1, 1, 0],
+        ],
+        "h": [
+            [1, 0, 0, 1],
+            [1, 0, 0, 1],
+            [1, 0, 0, 1],
+            [1, 1, 1, 1],
+            [1, 0, 0, 1],
+            [1, 0, 0, 1],
+            [1, 0, 0, 1],
+        ],
+        "i": [
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+        ],
+        "j": [
+            [1, 1, 1, 1],
+            [0, 0, 0, 1],
+            [0, 0, 0, 1],
+            [0, 0, 0, 1],
+            [0, 0, 0, 1],
+            [1, 0, 0, 1],
+            [0, 1, 1, 0],
+        ],
+        "k": [
+            [1, 0, 0, 0, 1],
+            [1, 0, 0, 1, 0],
+            [1, 0, 1, 0, 0],
+            [1, 1, 0, 0, 0],
+            [1, 0, 1, 0, 0],
+            [1, 0, 0, 1, 0],
+            [1, 0, 0, 0, 1],
+        ],
+        "l": [
+            [1, 0, 0, 0],
+            [1, 0, 0, 0],
+            [1, 0, 0, 0],
+            [1, 0, 0, 0],
+            [1, 0, 0, 0],
+            [1, 0, 0, 0],
+            [1, 1, 1, 1],
+        ],
+        "m": [
+            [1, 0, 0, 0, 1],
+            [1, 1, 0, 1, 1],
+            [1, 0, 1, 0, 1],
+            [1, 0, 0, 0, 1],
+            [1, 0, 0, 0, 1],
+            [1, 0, 0, 0, 1],
+            [1, 0, 0, 0, 1],
+        ],
+        "n": [
+            [1, 0, 0, 0, 1],
+            [1, 0, 0, 0, 1],
+            [1, 1, 0, 0, 1],
+            [1, 0, 1, 0, 1],
+            [1, 0, 0, 1, 1],
+            [1, 0, 0, 1, 1],
+            [1, 0, 0, 0, 1],
+        ],
+        "o": [
+            [0, 1, 1, 0],
+            [1, 0, 0, 1],
+            [1, 0, 0, 1],
+            [1, 0, 0, 1],
+            [1, 0, 0, 1],
+            [1, 0, 0, 1],
+            [0, 1, 1, 0],
+        ],
+        "p": [
+            [1, 1, 1, 0],
+            [1, 0, 0, 1],
+            [1, 0, 0, 1],
+            [1, 1, 1, 0],
+            [1, 0, 0, 0],
+            [1, 0, 0, 0],
+            [1, 0, 0, 0],
+        ],
+        "q": [
+            [0, 1, 1, 0],
+            [1, 0, 0, 1],
+            [1, 0, 0, 1],
+            [1, 0, 0, 1],
+            [1, 0, 0, 1],
+            [1, 0, 1, 0],
+            [0, 1, 0, 1],
+        ],
+        "r": [
+            [1, 1, 1, 1, 0],
+            [1, 0, 0, 0, 1],
+            [1, 0, 0, 0, 1],
+            [1, 1, 1, 1, 0],
+            [1, 0, 1, 0, 0],
+            [1, 0, 0, 1, 0],
+            [1, 0, 0, 0, 1],
+        ],
+        "s": [
+            [0, 1, 1, 0],
+            [1, 0, 0, 1],
+            [1, 0, 0, 0],
+            [0, 1, 1, 0],
+            [0, 0, 0, 1],
+            [1, 0, 0, 1],
+            [0, 1, 1, 0],
+        ],
+        "t": [
+            [1, 1, 1, 1, 1],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+        ],
+        "u": [
+            [1, 0, 0, 1],
+            [1, 0, 0, 1],
+            [1, 0, 0, 1],
+            [1, 0, 0, 1],
+            [1, 0, 0, 1],
+            [1, 0, 0, 1],
+            [0, 1, 1, 0],
+        ],
+        "v": [
+            [1, 0, 0, 0, 1],
+            [1, 0, 0, 0, 1],
+            [1, 0, 0, 0, 1],
+            [0, 1, 0, 1, 0],
+            [0, 1, 0, 1, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+        ],
+        "w": [
+            [1, 0, 0, 0, 1],
+            [1, 0, 0, 0, 1],
+            [1, 0, 0, 0, 1],
+            [1, 0, 1, 0, 1],
+            [1, 0, 1, 0, 1],
+            [1, 0, 1, 0, 1],
+            [0, 1, 0, 1, 0],
+        ],
+        "x": [
+            [1, 0, 0, 0, 1],
+            [1, 0, 0, 0, 1],
+            [0, 1, 0, 1, 0],
+            [0, 0, 1, 0, 0],
+            [0, 1, 0, 1, 0],
+            [1, 0, 0, 0, 1],
+            [1, 0, 0, 0, 1],
+        ],
+        "y": [
+            [1, 0, 0, 0, 1],
+            [1, 0, 0, 0, 1],
+            [0, 1, 0, 1, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+        ],
+        "z": [
+            [1, 1, 1, 1, 1],
+            [0, 0, 0, 0, 1],
+            [0, 0, 0, 1, 0],
+            [0, 0, 1, 0, 0],
+            [0, 1, 0, 0, 0],
+            [1, 0, 0, 0, 0],
+            [1, 1, 1, 1, 1],
+        ],
+    }
+
+
 def commitOnDate(date, numCommits):
     for i in range(numCommits):
         os.system('echo "." >> dots')
@@ -27,6 +266,55 @@ def writeLetters(letters):
                 startDate + datetime.timedelta(days=(letterOffset + offset)), 1)
         letterOffset += 35
 
+
+def generate_offset_list(letter):
+    rows = len(letter)
+    cols = len(letter[0])
+
+    offset_list = []
+    for i in range(rows):
+        for j in range(cols):
+            if letter[i][j]:
+                offset_list.append(rows * j + i)
+
+    return sorted(offset_list)
+
+
+def test_print_letter(letter):
+    rows = len(letter)
+    cols = len(letter[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            print("#", end="") if letter[i][j] else print(" ", end="")
+        print()
+
+
+letters = all_letters()
+
+letterA = generate_offset_list(letters["a"])
+letterB = generate_offset_list(letters["b"])
+letterC = generate_offset_list(letters["c"])
+letterD = generate_offset_list(letters["d"])
+letterF = generate_offset_list(letters["f"])
+letterG = generate_offset_list(letters["g"])
+letterI = generate_offset_list(letters["i"])
+letterJ = generate_offset_list(letters["j"])
+letterK = generate_offset_list(letters["k"])
+letterM = generate_offset_list(letters["m"])
+letterN = generate_offset_list(letters["n"])
+letterO = generate_offset_list(letters["o"])
+letterP = generate_offset_list(letters["p"])
+letterQ = generate_offset_list(letters["q"])
+letterR = generate_offset_list(letters["r"])
+letterS = generate_offset_list(letters["s"])
+letterT = generate_offset_list(letters["t"])
+letterU = generate_offset_list(letters["u"])
+letterV = generate_offset_list(letters["v"])
+letterW = generate_offset_list(letters["w"])
+letterX = generate_offset_list(letters["x"])
+letterY = generate_offset_list(letters["y"])
+letterZ = generate_offset_list(letters["z"])
 
 letterH = [0, 1, 2, 3, 4, 5, 6, 10, 17, 21, 22, 23, 24, 25, 26, 27]
 letterE = [0, 1, 2, 3, 4, 5, 6, 7, 10, 13, 14, 17, 20, 21, 24, 27]
