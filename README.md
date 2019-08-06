f7f178dbab4e215e5fcebc8ec9be8d9911aad60d
Jeffrey Fisher <jeff@slofish.io>
2019-08-05 20:19:52 -0700

    Allow arbitrary messages that are one word long.

--- README.md
+++ README.md
@@ -6,10 +6,16 @@
 
 ## Usage Instructions
 
-1. `git clone https://github.com/jeffslofish/contributions-hack.git`
-1. `cd contributions-hack`
-1. `python commit.py yyyy mm dd` 
-(Replace yyyy mm dd with your own year, month, and day for the first date of your first letter. Should be a Sunday.)
+In order for git commits to be counted towards your contributions, they cannot be from a fork, so please follow these instructions to make it work right:
+
+1. Create a new repository
+2. Clone your new repository
+3. `git clone https://github.com/jeffslofish/contributions-hack.git`
+4. `cp contributions-hack/* your-new-repo-name`
+5. `cd your-new-repo-name`
+6. `python commit.py message yyyy mm dd` (Replace message with your own word. Replace yyyy mm dd with your own year, month, and day for the first date of your first letter. Should be a Sunday. Also, your message will appear more clearly if you choose a date that has no contributions for a period of time after it, depending on how long your message is.)
+7. `git push` 
+8. Go to your user profile and see your message in the contributions area! (It usually only takes a few seconds for the commit contributions to appear after doing a push.)
 
 ## Notes
-- Currently the only letters that are defined are those in the word "HELLO". Feelf free to add more and submit a pull request!
+- Currently the only letters a-z are supported, and the message must be one word. Feel free to add more and submit a pull request!
--- commit.py
+++ commit.py
@@ -2,13 +2,16 @@ import os
 import sys
 import datetime
 
-if len(sys.argv) != 4:
-    print("Please enter three arguments: year month day. \nFor example, for Sept 2, 2018: python hack.py 2018 9 2")
+if len(sys.argv) != 5:
+    print("Please enter four arguments: message year month day. \nFor example, to print the message 'hello' for Sept 2, 2018: python commit.py hello 2018 9 2")
+    print("For the letters to line up correctly in GitHub, make sure the date is a Sunday")
+    print("Currently only the letters a - z are supported")
     sys.exit()
 
-year = int(sys.argv[1])
-month = int(sys.argv[2])
-day = int(sys.argv[3])
+message = sys.argv[1]
+year = int(sys.argv[2])
+month = int(sys.argv[3])
+day = int(sys.argv[4])
 startDate = datetime.date(year, month, day)
 
 
@@ -292,34 +295,8 @@ def test_print_letter(letter):
 
 letters = all_letters()
 
-letterA = generate_offset_list(letters["a"])
-letterB = generate_offset_list(letters["b"])
-letterC = generate_offset_list(letters["c"])
-letterD = generate_offset_list(letters["d"])
-letterF = generate_offset_list(letters["f"])
-letterG = generate_offset_list(letters["g"])
-letterI = generate_offset_list(letters["i"])
-letterJ = generate_offset_list(letters["j"])
-letterK = generate_offset_list(letters["k"])
-letterM = generate_offset_list(letters["m"])
-letterN = generate_offset_list(letters["n"])
-letterO = generate_offset_list(letters["o"])
-letterP = generate_offset_list(letters["p"])
-letterQ = generate_offset_list(letters["q"])
-letterR = generate_offset_list(letters["r"])
-letterS = generate_offset_list(letters["s"])
-letterT = generate_offset_list(letters["t"])
-letterU = generate_offset_list(letters["u"])
-letterV = generate_offset_list(letters["v"])
-letterW = generate_offset_list(letters["w"])
-letterX = generate_offset_list(letters["x"])
-letterY = generate_offset_list(letters["y"])
-letterZ = generate_offset_list(letters["z"])
+commitMessage = []
+for letter in message:
+    commitMessage.append(generate_offset_list(letters[letter]))
 
-letterH = [0, 1, 2, 3, 4, 5, 6, 10, 17, 21, 22, 23, 24, 25, 26, 27]
-letterE = [0, 1, 2, 3, 4, 5, 6, 7, 10, 13, 14, 17, 20, 21, 24, 27]
-letterL = [0, 1, 2, 3, 4, 5, 6, 13, 20, 27]
-letterO = [0, 1, 2, 3, 4, 5, 6, 7, 13, 14, 20, 21, 22, 23, 24, 25, 26, 27]
-wordHello = [letterH, letterE, letterL, letterL, letterO]
-
-writeLetters(wordHello)
+writeLetters(commitMessage)
